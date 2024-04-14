7ca0d4d7f1292f0b496b87729e61225bbdee6e7a
ZHallen122 <zhallen12261@gmail.com>
2024-04-14 15:13:59 -0400

    fix get log for cmd

--- logging/cowriecommand.py
+++ logging/cowriecommand.py
@@ -7,11 +7,12 @@ import base64
 def extract_commands(data, log_file):
     with open(log_file, 'r') as f:
         for line in f:
-            match = re.search(r'^.* \[twisted.conch.ssh.session#info\] Executing command "(.*)"', line)
+            # match = re.search(r'^.* \[twisted.conch.ssh.session#info\] Executing command "(.*)"', line)
+            match = re.search(r'CMD: \s*(.+)', line)
             if match:
-                command = match.groups()
-                data.append(command[0][2:-1])
-
+                # command = match.groups()
+                # data.append(command[0][2:-1])
+                data.append(match.group(1).strip())
 
 if __name__ == "__main__":
 
